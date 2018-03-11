8aeafc5d7fd6501bb5e31bce25e11e8154989d45
DivakarDon <divakar1408@gmail.com>
2018-03-11 07:43:35 +0530

    Initial commit

---
+++ app/src/main/java/com/suzhi/firebaseregistration/ResetPasswordActivity.java
@@ -0,0 +1,12 @@
+package com.suzhi.firebaseregistration;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ResetPasswordActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reset_password);
+    }
+}
